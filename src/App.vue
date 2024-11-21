b9e1c7b0ecfbf89a43b017b9b5af4af9b82ad0b2
NeilMagon <neil.magon97@gmail.com>
2024-11-21 12:06:39 +0100

    feat: creazione input e selected con funzione di props e emit da implementare

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <script setup>
-    import HelloWorld from './components/HelloWorld.vue'
+    import { ref } from 'vue'
+    import Input from './components/Input.vue'
+    const number = ref('')
+    const valuta = ref('EUR')
 </script>
 
 <template>
-    <div>
-    
+    <div class="my-cd">
+        <h1>Convertitore di value</h1>
+        <Input :value="number" :selected="valuta" @update:value="number = $event" @update:selected="valuta = $event"/>
     </div>
 </template>
 
 <style scoped  lang="scss">
-
+.my-cd{
+  text-align: center;
+  margin: 20px;
+}
 </style>
---
+++ src/components/Input.vue
@@ -0,0 +1,42 @@
+<script setup>
+    import { defineProps, defineEmits } from 'vue'
+    const name = 'Input';
+    const emit = defineEmits();
+    const props = defineProps({
+            value: {
+                type: Number,
+                default: ''
+            },
+            selected: {
+                type: String,
+                required: true
+            }
+        }
+    )
+</script>
+
+<template>
+    <div class="currency-input">
+        <input type="number" :value="props.value" @input="emit('update:value', $event.target.value)" placeholder="Inserieci il valore">
+        <select :value="props.selected" @change="emit('update:selected', $event.target.value)">
+            <option value="EUR">Euro (EUR)</option>
+            <option value="USD">Dollaro (USD)</option>
+            <option value="GBP">Sterlina (GBP)</option>
+        </select>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .currency-input {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        input {
+            width: 120px;
+            padding: 5px;
+        }
+        select {
+            padding: 5px;
+        }
+    }
+</style>
